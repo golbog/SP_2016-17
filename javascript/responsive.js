310fd0a85fc9095bd66ad76ecd5b89059a878b6a
Bogdan <bogdan.golobic@gmail.com>
2016-11-12 23:58:32 +0100

    Dodana SVG slika, dokončan footer, header in nav.

---
+++ javascript/responsive.js
@@ -0,0 +1,9 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function topnavResponsive() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "top-nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "top-nav";
+    }
+}
